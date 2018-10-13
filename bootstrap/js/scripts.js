5f4b8edb47aaf402abdf3ced7c885d27df0d3c9f
madoa <ciaran.kimberlin@gmail.com>
2018-10-13 14:11:45 +0100

    ""

--- bootstrap/js/scripts.js
+++ bootstrap/js/scripts.js
@@ -1,8 +1,22 @@
-function typeTitle() {
 
-}
 
 function logpi(){
+    let pi="3.141592653589793238462643383279502884197169399375105820974944";
+    for(i=0;i<1;i++) {
+        console.log(i)
+    }
+}
+
+/*(function checkPi() {
     let pi = "3.141592653589793238462643383279502884197169399375105820974944";
-    console.log(pi)
+    let test = document.forms["typePi"]["test"];
+    for(i=0;i<1;i++) {
+        console.log(test)
+    }
+    if (test.value == pi) {
+        alert("Spot on!");
+    }
+    else {
+        alert("Please try again");
+    }
 }
