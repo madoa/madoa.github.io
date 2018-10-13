b8fe5ed4d8e6e6b74d7e5e40a5f778ca6af4a609
madoa <ciaran.kimberlin@gmail.com>
2018-10-13 23:06:17 +0100

    ""

--- bootstrap/js/scripts.js
+++ bootstrap/js/scripts.js
@@ -8,6 +8,9 @@ function genCode() {
     else if(x == "") {
         document.getElementById("generatedCode").innerHTML = "Please enter an ID!";
     }
+    else if(x.length < 15){
+        document.getElementById("generatedCode").innerHTML = "Please enter a valid ID!";
+    }
     else if(y == ""){
         document.getElementById("generatedCode").innerHTML="Please enter a valid date!";
     }
