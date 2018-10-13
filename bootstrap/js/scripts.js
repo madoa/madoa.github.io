628c087459bcf0793d0fafbf551c7054560400cc
madoa <ciaran.kimberlin@gmail.com>
2018-10-13 14:39:16 +0100

    picheckermanual

--- bootstrap/js/scripts.js
+++ bootstrap/js/scripts.js
@@ -1,12 +1,24 @@
+function checkPi() {
+    let txt="";
+    let pi="3.141592653589793238462643383279502884197169399375105820974944";
+    if (document.getElementById("typePi").toString() == pi.toString()) {
+        txt = "Spot on!";
+    } else {
+
+        txt = "Please try again!";
+    }
+    document.getElementById("result").innerHTML = txt;
+}
 
 
-function logpi(){
+
+/*function logpi(){
     let pi="3.141592653589793238462643383279502884197169399375105820974944";
     for(i=0;i<1;i++) {
         console.log(i)
     }
 }
-
+*/
 /*(function checkPi() {
     let pi = "3.141592653589793238462643383279502884197169399375105820974944";
     let test = document.forms["typePi"]["test"];
@@ -20,3 +32,4 @@ function logpi(){
         alert("Please try again");
     }
 }
+*/
