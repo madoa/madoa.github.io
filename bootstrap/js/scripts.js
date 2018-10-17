99b87c64c670e3fb90e6810ebddb0378079d1daf
madoa <ciaran.kimberlin@gmail.com>
2018-10-18 00:29:26 +0100

    Update scripts.js

--- bootstrap/js/scripts.js
+++ bootstrap/js/scripts.js
@@ -21,6 +21,7 @@ function sortRef() { //v2, adds new line option
     let refList = document.getElementById("refInput").value.split('\n');//List of references to sort through/give back
     refList=refList.sort();//sort references into alphabetical order
     console.log(refList);//Debugging
+    //**if (document.getElementById().value checked, perform appropriate indentation
     for (let i = refList.length-1; i--;) {//remove any extra new lines
         if (refList[i] === "") refList.splice(i, 1);
     }
