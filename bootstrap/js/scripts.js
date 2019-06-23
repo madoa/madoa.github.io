8ae97f2b88c7cebf5d8f40048e2b11424bc0ad4d
madoa <c@kimbo.space>
2019-06-23 10:59:43 +0100

    Add board game bazaar and crypto page

--- bootstrap/js/scripts.js
+++ bootstrap/js/scripts.js
@@ -97,3 +97,144 @@ function getTxt(x) {//GETs robots.txt contents
 
 
 */
+/*{
+    "Title":"",
+    "img":"",
+    "inside":"",
+    "price":""
+},
+{
+        "Title":"",
+        "img":"",
+        "text":"",
+        "price":""
+    }*/
+//list of products
+bgList = [
+    {
+        "Title":"Cards Against Humanity",
+        "img":"bg0.jpg",
+        "inside":"bg0i.jpg",
+        "price":""
+    },
+    {
+        "Title":"Exploding Kittens",
+        "img":"bg1.jpg",
+        "inside":"bg1i.jpg",
+        "price":""
+    },
+    {
+        "Title":"Settlers of Catan",
+        "img":"bg2.jpg",
+        "inside":"bg2i.jpg",
+        "price":""
+    },
+    {
+        "Title":"Monopoly",
+        "img":"bg3.jpg",
+        "inside":"bg3i.jpg",
+        "price":""
+    },
+    {
+        "Title":"Town of Salem",
+        "img":"bg4.jpg",
+        "inside":"bg4i.jpg",
+        "price":""
+    },
+    {
+        "Title":"Pandemic",
+        "img":"bg5.jpg",
+        "inside":"bg5i.jpg",
+        "price":""
+    },
+    {
+        "Title":"Ticket To Ride",
+        "img":"bg6.jpg",
+        "inside":"bg6i.jpg",
+        "price":""
+    },
+    {
+        "Title":"Castle Panic",
+        "img":"bg7.jpg",
+        "inside":"bg7i.jpg",
+        "price":""
+    },
+    {
+        "Title":"Guess Who?",
+        "img":"bg8.jpg",
+        "inside":"bg8i.jpg",
+        "price":""
+    },
+    {
+        "Title":"Operation",
+        "img":"bg9.jpg",
+        "inside":"bg9i.jpg",
+        "price":""
+    },
+    {
+        "Title":"Mouse Trap",
+        "img":"bg10.jpg",
+        "inside":"bg10i.jpg",
+        "price":""
+    },
+    //items that don't have what's inside
+    {
+        "Title":"Food",
+        "img":"bg11.jpeg",
+        "text":"",
+        "price":""
+    },
+    {
+        "Title":"Ludo spares",
+        "img":"bg12.jpeg",
+        "text":"",
+        "price":""
+    },
+    {
+        "Title":"Poker",
+        "img":"bg13.jpeg",
+        "text":"",
+        "price":""
+    },
+    {
+        "Title":"Scrabble spares",
+        "img":"bg14.jpeg",
+        "text":"",
+        "price":""
+    },
+    {
+        "Title":"Plain ol' dice",
+        "img":"bg15.jpeg",
+        "text":"",
+        "price":""
+    },
+    {
+        "Title":"Monopoly spares",
+        "img":"bg16.jpeg",
+        "text":"",
+        "price":""
+    },
+];
+
+cList = {
+    "BTC":{
+        "desc":"Check out more info at <a href='https://bitcoin.org/en/'>Bitcoin.org</a>",
+        "fn":"Bitcoin"
+    },
+    "LTC":{
+        "desc":"Check out more info at <a href='https://litecoin.org/'>Litecoin.org</a>",
+        "fn":"Litecoin"
+    },
+    "ETH":{
+        "desc":"Check out more info at <a href='https://www.ethereum.org/'>Ethereum.org</a>",
+        "fn":"Ethereum"
+    },
+    "XRP":{
+        "desc":"Check out more info at <a href='https://ripple.com/'>Ripple.com</a>",
+        "fn":"Ripple"
+    },
+    "DGE":{
+        "desc":"Check out more info at <a href='https://dogecoin.com/'>dogecoin.com</a>",
+        "fn":"Dogecoin"
+    }
+};
